f1fe59e94248c93464a26341d2d7db8d963faa06
Juya <juyaamirm@gmail.com>
2024-08-21 11:26:00 +0200

    array message is added

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom"
+import React, {useEffect, useState} from 'react'
 
 const Home = () => {
+  const message = [
+    "Full Stack Web Developer", "IT Engineer", " and a Tech Enthusiast"
+  ];
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentMessageIndex((prevIndex) => (prevIndex +1) % message.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [message.length]);
   return (
     <section id='home' className='text-center mt-6 p-2 md:p-4  lg:p-8'>
 
@@ -9,23 +20,12 @@ const Home = () => {
         <span className="text-blue-500"> Amir Muhammad Juya</span>
       </h1>
       <div className="block mx-auto w-max">
-        <div className="typed-out type1">
-          <h2 className="lg:text-3xl md:text-2xl text-xl my-4 introduction ">Full Stack Web Developer </h2>
+        <div className="typed-out">
+        <h2 className="lg:text-3xl md:text-2xl text-xl my-4 introduction text-start"> 
+          {message[currentMessageIndex]}
+        </h2>
         </div>
       </div>
-{/* 
-      <div className="block mx-auto w-max">
-        <div className="typed-out type2">
-          <h2 className="lg:text-3xl md:text-2xl text-xl my-4">IT Engineer</h2>
-        </div>
-      </div>
-      
-      <div className="block w-max mx-auto">
-        <div className="typed-out type3">
-          <h2 className="lg:text-3xl md:text-2xl text-xl my-4">Tech Enthusiast</h2>
-        </div>
-      </div> */}
-
       <div className="mt-10">
         <Link to='https://drive.google.com/file/d/1VJJseYsfq1Lu0uZdTaqgFPocZj_0P1oF/view?usp=drive_link' target="_blank">
           <button type="submit" className="btn text-blue-500 text-lg font-bold  border-2 border-blue-500 bg-white hover:bg-blue-500 hover:text-white px-6 md:px-12 lg:px-16   
