851134d3e0443d71d744276440dc32c34bf0e3e3
Juya <juyaamirm@gmail.com>
2024-10-12 00:25:26 +0200

    deploy

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           <img src={pic} alt="My Picture" className="w-64 border rounded-full border-blue-500 ring-4 ring-inherit" />
         </figure>
         <p className="text-start text-xl max-w-xl leading-relaxed ">
-          Hi, I'm Amir Muhammad Juya, a full-stack developer with a passion for solving complex challenges and delivering outstanding user experiences. With over four years of IT experience, I’ve honed my skills in JavaScript, React, Node.js, MongoDB, and more, which I’ve applied in projects like JourneyPack—an AI-powered e-commerce platform. My expertise lies in crafting exceptional web applications that translate client ideas into reality.
+          Hi, I'm Amir Juya, a full-stack developer with a passion for solving complex challenges and delivering outstanding user experiences. With over four years of IT experience, I’ve honed my skills in JavaScript, React, Node.js, MongoDB, and more, which I’ve applied in projects like JourneyPack—an AI-powered e-commerce platform. My expertise lies in crafting exceptional web applications that translate client ideas into reality.
           I'm a continuous learner and tech enthusiast who thrives in team-oriented, agile environments. My ultimate goal is to create top-notch products that not only meet but exceed expectations.
           <span className="block text-xl font-medium border-b-2 p-1 my-4 border-blue-600 hover:bg-blue-600 hover:text-white w-fit italic ">
             <Link smooth to='#contact'>Contact!</Link>
